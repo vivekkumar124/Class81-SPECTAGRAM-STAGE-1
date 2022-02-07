bf08260072fe6d43b61f17a9f46908d10a8a82d4
vivekkumar124 <76097909+vivekkumar124@users.noreply.github.com>
2022-02-07 17:05:48 +0530

    Add files via upload

---
+++ screens/CreatePost.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class CreatePost extends React.Component {
+
+  render() {
+    return (
+      <View style = {{flex : 1}}>
+
+       <Text> CreatePost </Text>
+      </View>
+    ); 
+  }
+}
+
---
+++ screens/Feed.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class Feed extends React.Component {
+
+  render() {
+    return (
+      <View style = {{flex : 1}}>
+
+       <Text> Feed </Text>
+      </View>
+    ); 
+  }
+}
+
